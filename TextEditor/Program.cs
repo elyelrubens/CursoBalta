ee3d8f4880282dda632594c63d8ac771f61ef7e9
Elyel Rubens da Rosa <elyelrubens@gmail.com>
2021-11-02 09:57:10 -0300

    Finailizado Editor de textos

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Olá, por favor escolha oque deseja fazer");
+    Console.WriteLine("1 - Abrir um arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default: Menu(); break;
+    }
+
+    static void Open()
+    {
+        Console.Clear();
+        Console.WriteLine("Informe o caminho do arquivo que desja abrir");
+        string path = Console.ReadLine();
+
+        using (var file = new StreamReader(path))
+        {
+            string text = file.ReadToEnd();
+            Console.WriteLine(text);
+        }
+
+        Console.WriteLine("");
+        Console.ReadLine();
+        Menu();
+    }
+
+    static void Edit()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("------------------------------");
+
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Save(text);
+    }
+
+    static void Save(string text)
+    {
+        Console.Clear();
+        Console.WriteLine("Informe o caminho para salvar o arquivo");
+        var path = Console.ReadLine();
+
+        using (var file = new StreamWriter(path))
+        {
+            file.Write(text);
+        }
+
+        Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+        Console.ReadLine();
+        Menu();
+    }
+}
